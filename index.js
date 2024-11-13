4c752a6832d1c46d71427493023db03c3ad4dfaa
aayman01 <ayman.alfi098@gmail.com>
2024-11-13 10:14:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Defining State
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_USER = 'ADD_USER';
+
+
+// state 
+const initialCounterState = {
+    count : 0
+}
+const initialUserState = {
+    user : [
+        {name : 'Aayman'}
+    ]
+}
+
+// Action - object - type, payload
+
+const incrementCounter = () =>{
+    return {
+      type: INCREMENT,
+    };
+}
+const decrementCounter = () =>{
+    return {
+      type: DECREMENT,
+    };
+}
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload : {user : "Abrar"}
+  };
+};
